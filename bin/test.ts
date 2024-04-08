57c774b59a38bf32ea44f18d284ab260afd5cc16
Fernando Isidro Luna <luffynando@gmail.com>
2024-04-07 21:23:19 -0600

    chore: :heavy_plus_sign: move to release-it instead of np and remove unused packages

--- bin/test.ts
+++ bin/test.ts
@@ -1,5 +1,4 @@
 import { assert } from '@japa/assert';
-import { expectTypeOf } from '@japa/expect-type';
 import { fileSystem } from '@japa/file-system';
 import { configure, processCLIArgs, run } from '@japa/runner';
 import { BASE_URL } from '../tests_helpers/helpers.js';
@@ -8,7 +7,7 @@ processCLIArgs(process.argv.splice(2));
 
 configure({
   files: ['tests/**/*.spec.ts'],
-  plugins: [assert(), fileSystem({ basePath: BASE_URL }), expectTypeOf()],
+  plugins: [assert(), fileSystem({ basePath: BASE_URL })],
 });
 
 run();
